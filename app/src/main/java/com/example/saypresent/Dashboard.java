6ed57b9172ecbd702c3b4d02c55521142a96a5b2
chrisjoohn <chrisjohnmulingbayan@gmail.com>
2020-01-23 00:59:23 +0800

    UPDATE: welcome user with first name

--- app/src/main/java/com/example/saypresent/Dashboard.java
+++ app/src/main/java/com/example/saypresent/Dashboard.java
@@ -1,14 +1,23 @@
 package com.example.saypresent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.saypresent.controller.OrganizerController;
+import com.example.saypresent.database.Database;
 import com.example.saypresent.model.Organizer;
 import com.example.saypresent.utils.GetOrganizerInterface;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +26,9 @@ public class Dashboard extends AppCompatActivity {
     private Organizer organizerAttribute;
     private OrganizerController organizerController;
     private TextView dashboard_name;
+    private GetOrganizerInterface getOrganizerInterface;
+    private Database database = new Database();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +36,46 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         Button addEvent;
 
+        dashboard_name = (TextView) findViewById(R.id.dashboardFN);
+
         Intent intent = getIntent();
         this.organizer_key = intent.getStringExtra("organizer_key");
-        setOrganizer(organizer_key);
 
         addEvent = findViewById(R.id.addEvent);
         addEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (Dashboard.this,addEvent.class);
+                intent.putExtra("organizer_key", organizer_key);
                 startActivity(intent);
+            }
+        });
+        setName(this.organizer_key);
+
+
+    }
+
+    private void setName(final String organizer_key){
+
+        DatabaseReference organizerReference = database.organizerRef;
+        organizerReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for (DataSnapshot organizerSnapshot : dataSnapshot.getChildren()){
+                        Organizer organizer = organizerSnapshot.getValue(Organizer.class);
+                        if(organizerSnapshot.getKey().equals(organizer_key)){
+                            dashboard_name.setText(organizer.getFirst_name());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("error", databaseError.getMessage());
             }
         });
     }
 
-    private void setOrganizer(String organizer_key){
-        dashboard_name = (TextView) findViewById(R.id.dashboardFN);
-        GetOrganizerInterface getOrganizerInterface = new GetOrganizerInterface() {
-            @Override
-            public void onGetOrganizer(Organizer organizer) {
-                organizerAttribute = organizer;
-                dashboard_name.setText(organizer.getFirst_name());
-            }
-        };
-        organizerController = new OrganizerController();
-        organizerController.getOrganizer(organizer_key, getOrganizerInterface);
-    }
 }
