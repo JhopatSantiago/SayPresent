84569bea72eb45c89405e4a220b150a3fdf8be27
JhopatSantiago <46297312+JhopatSantiago@users.noreply.github.com>
2020-01-22 16:00:17 +0800

    UPDATE: January 22, 2020
    - Change in splash animation layout
    - Create Add Event (connecting event.xml to dashboard)

--- app/src/main/java/com/example/saypresent/Dashboard.java
+++ app/src/main/java/com/example/saypresent/Dashboard.java
@@ -2,13 +2,25 @@ package com.example.saypresent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
-
+    Button addEvent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        addEvent = findViewById(R.id.addEvent);
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Dashboard.this,addEvent.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/saypresent/MainActivity.java
+++ app/src/main/java/com/example/saypresent/MainActivity.java
@@ -6,18 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.saypresent.datastore.DataStore;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
 
-
+    private ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = (ImageView)findViewById(R.id.whiteLogo);
+        Animation myanim = AnimationUtils.loadAnimation(MainActivity.this,R.anim.splashanimation);
+        logo.startAnimation(myanim);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
