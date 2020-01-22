6af680921f2b3b5ac931f913fc6c3a6856588312
chrisjoohn <chrisjohnmulingbayan@gmail.com>
2020-01-22 22:21:33 +0800

    Create GetOrganizerInterface.java

---
+++ app/src/main/java/com/example/saypresent/utils/GetOrganizerInterface.java
@@ -0,0 +1,6 @@
+package com.example.saypresent.utils;
+import com.example.saypresent.model.Organizer;
+
+public interface GetOrganizerInterface {
+    public void onGetOrganizer(Organizer organizer);
+}
