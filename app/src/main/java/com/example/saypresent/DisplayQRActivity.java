48288e6912c70d40030dc74ccdd751ede53261e5
chrisjoohn <chrisjohnmulingbayan@gmail.com>
2020-01-25 16:02:45 +0800

    UPDATE: Added QR code generator and viewer for Event Attendees

---
+++ app/src/main/java/com/example/saypresent/DisplayQRActivity.java
@@ -0,0 +1,114 @@
+package com.example.saypresent;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class DisplayQRActivity extends AppCompatActivity {
+    private String attendee_key;
+    private ImageView imageHolder;
+    private Bitmap qrImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_qr);
+
+
+        /**
+         * ACTUAL CODE
+         */
+//        Intent intent = getIntent();
+//        this.attendee_key = intent.getStringExtra("attendee_key");
+
+        /**
+         * DUMMY DATA - FOR SAMPLE
+         */
+        this.attendee_key = "Hello world!";
+
+        imageHolder = (ImageView) findViewById(R.id.qr_image);
+
+//        Log.i("tag", "I'm here");
+        generateQRCode();
+    }
+
+    private void alert(String message){
+        AlertDialog dlg = new AlertDialog.Builder(this)
+                .setTitle("QRCode Generator")
+                .setMessage(message)
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        dlg.show();
+    }
+
+    private void generateQRCode(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int size = 260;
+                Log.i("attendee_key", attendee_key);
+                Map<EncodeHintType, Object> hintMap = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+                hintMap.put(EncodeHintType.CHARACTER_SET, "utf-8");
+                hintMap.put(EncodeHintType.MARGIN, 1);
+
+                QRCodeWriter qrCodeWriter = new QRCodeWriter();
+                try{
+                    BitMatrix bitMatrix = qrCodeWriter.encode(attendee_key, BarcodeFormat.QR_CODE, size, size, hintMap);
+                    int height = bitMatrix.getHeight();
+                    int width = bitMatrix.getWidth();
+                    qrImage = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+
+                    for (int x=0; x<width; x++){
+                        for(int y=0; y<height; y++){
+                            qrImage.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                        }
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showImage(qrImage);
+
+                        }
+                    });
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                    alert(e.getMessage());
+                }
+
+            }
+        }).start();
+    }
+
+    private void showImage(Bitmap bitmap) {
+        if (bitmap == null) {
+            imageHolder.setImageResource(android.R.color.transparent);
+            qrImage = null;
+        } else {
+            imageHolder.setImageBitmap(bitmap);
+        }
+    }
+
+}
