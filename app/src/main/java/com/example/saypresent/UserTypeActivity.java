58345dd883925c83af145a700a8f0e62e82feb35
JhopatSantiago <46297312+JhopatSantiago@users.noreply.github.com>
2020-02-22 11:24:46 +0800

    UPDATE: User Type UI

--- app/src/main/java/com/example/saypresent/UserTypeActivity.java
+++ app/src/main/java/com/example/saypresent/UserTypeActivity.java
@@ -1,8 +1,10 @@
 package com.example.saypresent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +17,13 @@ public class UserTypeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_type);
 
+        setContentView(R.layout.activity_user_type);
+        ConstraintLayout constraintLayout = findViewById(R.id.Layout3);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
         organizer_btn = (Button) findViewById(R.id.organizer_button);
         attendee_btn = (Button) findViewById(R.id.attendee_button);
 
